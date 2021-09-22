ef46b7cbecb68c3ada3d99ef2484f85393fe8d99
Jacarder <jonandrewcarder@gmail.com>
2021-09-21 20:18:19 -0400

    created app for typescript

---
+++ coriolis-companion/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div className="Header" data-testid="Header">
+    Header Component
+  </div>
+);
+
+export default Header;
