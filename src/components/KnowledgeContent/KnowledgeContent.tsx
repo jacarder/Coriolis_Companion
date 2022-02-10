31cb9b61f10f8aadb18a3874285ddb2233f6538e
Jacarder <jonandrewcarder@gmail.com>
2022-02-10 11:13:48 -0500

    More fluid transitions in and out

--- src/components/KnowledgeContent/KnowledgeContent.tsx
+++ src/components/KnowledgeContent/KnowledgeContent.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Fade, Typography } from '@mui/material';
 import React, { FC, useEffect, useState } from 'react';
 import { IKnowledgeItem } from '../../interfaces/knowledge-item';
 import KnowledgeCenterService from '../../services/KnowledgeCenterService';
@@ -10,29 +10,34 @@ interface KnowledgeContentProps {
 
 const KnowledgeContent: FC<KnowledgeContentProps> = ({dataId}) => {
   const [data, setData] = useState<IKnowledgeItem | undefined>(undefined);
+  const [open, setOpen] = useState<boolean>(false);
   useEffect(() => {
-    setData(KnowledgeCenterService.getSectionData(dataId));
+    setOpen(false);
+    setTimeout(() => {
+      setData(KnowledgeCenterService.getSectionData(dataId));
+      setOpen(true)
+    }, 200);
   }, [dataId]);
   
   return (
-    <>    
-    <Box display={'flex'} flexDirection={'column'}>
-      <Typography variant="h3" component="div" sx={{alignSelf: 'center'}}>
-        {data?.title}
-      </Typography>
-      <Typography 
-        variant="subtitle1" 
-        component="div" 
-        sx={{alignSelf: 'center'}} 
-        gutterBottom
-      >
-        {data?.subTitle}
-      </Typography>
-      <Typography variant="body1" component="div">
-        {data?.content}
-      </Typography>       
-    </Box>
-    </>
+    <Fade in={open} timeout={{enter: 500, exit: 200}}>
+      <Box display={'flex'} flexDirection={'column'}>
+          <Typography variant="h3" component="div" sx={{alignSelf: 'center'}}>
+            {data?.title}
+          </Typography>
+          <Typography 
+            variant="subtitle1" 
+            component="div" 
+            sx={{alignSelf: 'center'}} 
+            gutterBottom
+          >
+            {data?.subTitle}
+          </Typography>
+          <Typography variant="body1" component="div">
+            {data?.content}
+          </Typography>  
+      </Box>
+    </Fade>
   )
 }
 
