7c61c12377b99f2ce1dfa2dc5955974b52e11e7d
Jacarder <jonandrewcarder@gmail.com>
2022-03-15 18:56:09 -0400

    Home page welcome

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Typography } from "@mui/material";
 import { useEffect } from "react";
 import IPage from "../interfaces/page";
 
@@ -7,7 +8,19 @@ const HomePage: React.FunctionComponent<IPage> = props => {
 		
 	}, [])
 	return (
-		<p>This is the home page</p>
+		<>
+			<Box mt='8px'>
+				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h3" gutterBottom >
+					Welcome to
+				</Typography>
+				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h1" gutterBottom >
+					The Third Horizon
+				</Typography>
+				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h5" gutterBottom >
+					May the Icons protect
+				</Typography>			
+			</Box>
+		</>
 	)
 }
 
