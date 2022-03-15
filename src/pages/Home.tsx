25ba9f89492f8ee128bdbdc4a392fa79d23814b4
Jacarder <jonandrewcarder@gmail.com>
2022-03-15 19:25:14 -0400

    Animation added to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grow, Typography, Fade } from "@mui/material";
 import { useEffect } from "react";
 import IPage from "../interfaces/page";
 
@@ -8,19 +8,23 @@ const HomePage: React.FunctionComponent<IPage> = props => {
 		
 	}, [])
 	return (
-		<>
-			<Box mt='8px'>
+		<Box pt='30px' alignItems={'center'} sx={{display: 'flex', flexDirection: 'column'}}>
+			<Grow in timeout={1000} unmountOnExit>
 				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h3" gutterBottom >
 					Welcome to
 				</Typography>
+			</Grow>
+			<Fade in timeout={1000} style={{transitionDelay: '800ms'}} unmountOnExit>
 				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h1" gutterBottom >
 					The Third Horizon
 				</Typography>
+			</Fade>
+			<Fade in timeout={1000} style={{transitionDelay: '1600ms'}} unmountOnExit>
 				<Typography className="welcome-font" textAlign={'center'} color='white' variant="h5" gutterBottom >
 					May the Icons protect
-				</Typography>			
-			</Box>
-		</>
+				</Typography>
+			</Fade>						
+		</Box>
 	)
 }
 
