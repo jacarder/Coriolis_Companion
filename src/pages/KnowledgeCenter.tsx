65fb2121fcb90c988ba5a576bfd5b96c33baece2
Jacarder <jonandrewcarder@gmail.com>
2022-07-06 20:55:37 -0400

    Removed xs from Grid not using item prop

--- src/pages/KnowledgeCenter.tsx
+++ src/pages/KnowledgeCenter.tsx
@@ -1,10 +1,8 @@
 import { Grid, Paper } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import KnowledgeContent from "../components/KnowledgeContent/KnowledgeContent";
 import TableOfContents from "../components/TableOfContents/TableOfContents";
-import { BazaarCategory } from "../constants/BazaarCategory";
 import IPage from "../interfaces/page";
-import Bazaar from "./Bazaar";
 
 
 const KnowledgeCenterPage: React.FunctionComponent<IPage> = props => {
@@ -13,11 +11,11 @@ const KnowledgeCenterPage: React.FunctionComponent<IPage> = props => {
 		setDataId(selectedDataId);
 	}
 	return (
-		<Grid container xs={12} spacing={1} component={Paper} sx={{marginLeft: 0, marginTop: 0}}>
-			<Grid item xs={3} sx={{borderRight: 'grey solid 1px'}}>				
+		<Grid container spacing={1} component={Paper} sx={{ marginLeft: 0, marginTop: 0 }}>
+			<Grid item xs={3} sx={{ borderRight: 'grey solid 1px' }}>
 				<TableOfContents selectSection={selectSection}></TableOfContents>
 			</Grid>
-			<Grid item xs={9}>				
+			<Grid item xs={9}>
 				<KnowledgeContent dataId={dataId}></KnowledgeContent>
 			</Grid>
 		</Grid>
