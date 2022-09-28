20e76c3402602f8b8a663aab64a487794dfdda90
Jacarder <jonandrewcarder@gmail.com>
2022-09-28 01:02:02 -0400

    Continued work with generator service
    Returning ICharacter object

--- src/services/GenerateNPC.service.ts
+++ src/services/GenerateNPC.service.ts
@@ -1,9 +1,21 @@
-import { CHARACTER_CONCEPT, ICharacterSubConcept } from "../interfaces/character";
+import { CHARACTER_CONCEPT, ICharacter, ICharacterSubConcept } from "../interfaces/character";
 import * as _ from 'lodash';
+import GeneratePersonService from "./GeneratePerson.service";
+import { uniqueId } from "lodash";
 
-export const generateNPC = () => {
-	const characterConcept = _.sample(Object.values(CHARACTER_CONCEPT)),
-		characterSubConcept = _.sample(getSubConcepts().filter(x => x.parentConcept === characterConcept));
-	console.log(characterConcept, characterSubConcept)
+export const generateNPC = async (): Promise<ICharacter> => {
+	const characterConcept = _.sample(Object.values(CHARACTER_CONCEPT)) as CHARACTER_CONCEPT,
+		characterSubConcept = _.sample(getSubConcepts().filter(x => x.parentConcept === characterConcept)) as ICharacterSubConcept,
+		{ firstName, lastName } = await GeneratePersonService.getNames("m");
+
+	return {
+		id: uniqueId(),
+		name: `${firstName} ${lastName}`,
+		concept: characterConcept,
+		subConcept: characterSubConcept,
+		description: '', // TODO generate
+		features: [] // TODO generate
+	}
+
 }
 const getSubConcepts = (): ICharacterSubConcept[] => require('../json/character-creation.json').subConcepts;
