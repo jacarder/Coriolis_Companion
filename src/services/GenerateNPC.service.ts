32ccb88dd0c74a8b3a86ce0121f5f6e92d59b416
Jacarder <jonandrewcarder@gmail.com>
2022-09-28 20:45:26 -0400

    Creating speech pattern and characteristics with openAI

--- src/services/GenerateNPC.service.ts
+++ src/services/GenerateNPC.service.ts
@@ -2,19 +2,24 @@ import { CHARACTER_CONCEPT, ICharacter, ICharacterSubConcept } from "../interfac
 import * as _ from 'lodash';
 import GeneratePersonService from "./GeneratePerson.service";
 import { uniqueId } from "lodash";
+import { createText } from "./OpenAI.service";
+import { OPENAI_COMMAND } from "../constants/OpenAI";
 
 export const generateNPC = async (): Promise<ICharacter> => {
 	const characterConcept = _.sample(Object.values(CHARACTER_CONCEPT)) as CHARACTER_CONCEPT,
 		characterSubConcept = _.sample(getSubConcepts().filter(x => x.parentConcept === characterConcept)) as ICharacterSubConcept,
-		{ firstName, lastName } = await GeneratePersonService.getNames("m");
+		{ firstName, lastName } = await GeneratePersonService.getNames("m"),
+		{ data: { choices: characteristics } } = await createText(OPENAI_COMMAND.CREATE_CHARACTER_CHARACTERISTICS),
+		{ data: { choices: speechPattern } } = await createText(OPENAI_COMMAND.CREATE_CHARACTER_SPEECH_PATTERN);
 
 	return {
 		id: uniqueId(),
 		name: `${firstName} ${lastName}`,
 		concept: characterConcept,
 		subConcept: characterSubConcept,
-		description: '', // TODO generate
-		features: [] // TODO generate
+		description: '', //TODO generate
+		features: characteristics?.length === 1 ? (characteristics[0].text ?? '') : '',
+		speechPattern: speechPattern?.length === 1 ? (speechPattern[0].text ?? '') : ''
 	}
 
 }
